b01a28e19d2958ef954fc12dc2bdcda1b9bfe972
FroxenMC <hgggopfunnu@gmail.com>
2026-05-22 19:56:23 +0530

    Create memories.js

---
+++ memories.js
@@ -0,0 +1,23 @@
+const memories=[
+
+"📷 First Chat",
+
+"💞 First Memory",
+
+"✨ First Chaos"
+
+]
+
+memories.forEach(x=>{
+
+memoryGrid.innerHTML+=`
+
+<div class="card">
+
+${x}
+
+</div>
+
+`
+
+})
